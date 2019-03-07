f69f29672a3ee78b87e62a4ab018a896b3c37696
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2019-03-07 13:55:39 -0600

    Add files via upload

---
+++ toggleFunctions.js
@@ -0,0 +1,46 @@
+const doc= document
+function green25(){
+    let box = doc.querySelectorall("div");
+    for(let b=0; b < box.length; b++){
+        box.style.fonstsize = green;
+    } 
+}
+
+      
+}
+
+
+function green50(){
+    let h3 = doc.querySelector("h3"); 
+        h3[g].style.fonstsize = "medium";
+}
+
+
+function green75(){
+    let h3 = doc.querySelector("h3"); 
+    for(let g=0; g<green.clientHeight; g++){
+        h3[g].style.fonstsize = "medium";
+
+}
+
+
+function green100(){
+
+
+}
+
+
+function smallToggle(){
+
+
+}
+
+
+function mediumToggle(){
+
+}
+
+
+function largeToggle(){
+
+}
