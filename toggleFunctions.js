5d3d0232f65315322fe222d9add9b09e2cf9004f
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2019-03-08 13:50:24 -0600

    Add files via upload

--- toggleFunctions.js
+++ toggleFunctions.js
@@ -1,37 +1,50 @@
-const doc= document
+
 function green25(){
-    let box = doc.querySelectorall("div");
-    for(let b=0; b < box.length; b++){
-        box.style.fonstsize = green;
+    const doc= document;
+    let boxes = doc.querySelectorAll(".boxes > div");
+    for(let b=0; b < boxes.length; b++){
+        boxes[b].classList.add("green25");
     } 
 }
 
-      
-}
+    
 
 
 function green50(){
-    let h3 = doc.querySelector("h3"); 
-        h3[g].style.fonstsize = "medium";
+    const doc= document;
+    let boxes = doc.querySelectorAll(".boxes > .div");
+    for(let b=0; b < boxes.length; b++){
+        boxes[b].classlist.add = "green50";
+    } 
 }
+   
 
 
 function green75(){
-    let h3 = doc.querySelector("h3"); 
-    for(let g=0; g<green.clientHeight; g++){
-        h3[g].style.fonstsize = "medium";
+    const doc= document;
+    let boxes = doc.querySelectorall(".boxes > .div");
+    for(let b=0; b < boxes.length; b++){
+        boxes[b].classlist.add = "green50";
+       
 
 }
 
 
 function green100(){
+    const doc= document
+    let box = doc.querySelectorall(".box > .div");
+    for(let b=0; b < box.length; b++){
+        box[b].classlist.add = "green50";
 
 
 }
 
 
 function smallToggle(){
-
+    for(let b=0; b<links.length; b++){
+        links[b].classlist.toggle(green);
+        links[b].classlist.toggle(red);
+}
 
 }
 
